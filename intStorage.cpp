06041bde03904ae34970d6f450b78b50ac04bc59
VasiliiSokolov <vasilysokolov@mail.ru>
2022-09-13 20:56:22 +0300

    Добавьте файлы проекта.

---
+++ intStorage.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+void shift_vector(std::vector<int>&);
+
+int main()
+{
+    int input;
+    std::vector<int> db;
+
+    for (int i = 0;; i++)
+    {
+        if (i == 20) i = 19;
+        std::cout << "input number: ";
+        std::cin >> input;
+        if (input == -2) break;
+        if (input == -1)
+        {
+            std::cout << "output: ";
+            for (int l = 0; l < db.size(); l++)
+            {
+                std::cout << db[l] << " ";
+            }
+            std::cout << "\n";
+            i--;
+            input = db[i];
+        }
+        db.push_back(input);
+        
+    }
+}
+
+void shift_vector(std::vector<int>& db)
+{
+    for (int i = 1; i < db.size(); i++)
+    {
+        db[i] = db[i - 1];
+    }
+}
